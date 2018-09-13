afc916bb23fc6c076aefadb874af7b6abd90aa0e
Lemony <lemonystudio@gmail.com>
2018-09-13 12:54:48 +0100

    got text to display on top of timeline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const formSubmit = document.querySelector("form");
-
 formSubmit.addEventListener("submit", event => {
-    event.preventDefault();
-})
+  event.preventDefault();
+
+  const childNode = document.querySelector("#messagebox");
+  const tweetArea = document.querySelector(".tweet--area");
+
+  childNode.value = "";
+
+  const containerNode = document.createElement("p");
+  const textContent = document.createTextNode(childNode.value);
+
+  containerNode.prepend(textContent);
+  tweetArea.prepend(containerNode);
+});
